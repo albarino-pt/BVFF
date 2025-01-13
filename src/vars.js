61c60c3fc2e88cffe2b188b4262923e0443ddb03
albarino-pt <francisco@albarino.pt>
2025-01-13 11:46:52 +0000

    11.46-13.1.2025
    not complete. but ready to show

---
+++ src/vars.js
@@ -0,0 +1,22 @@
+export const calculateAge = (birthDate) => {
+    const currentDate = new Date(); // Get the current date
+    const birthDateObj = new Date(birthDate); // Convert the birth date to a Date object
+  
+    // Calculate age based on the year difference
+    let age = currentDate.getFullYear() - birthDateObj.getFullYear();
+    const monthDifference = currentDate.getMonth() - birthDateObj.getMonth();
+  
+    // If the current month is before the birth month, or it's the birth month but before the birthday, subtract 1
+    if (monthDifference < 0 || (monthDifference === 0 && currentDate.getDate() < birthDateObj.getDate())) {
+      age--;
+    }
+  
+    return age;
+  };
+
+export const birthDate = "1882-12-19"; // Example birthdate (Year-Month-Day)
+
+export const emergencias = "1043";
+export const kmperco = "52145";
+export const qab = "104";
+export const age = calculateAge(birthDate);
