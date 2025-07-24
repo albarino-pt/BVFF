7e0aa3567bd5ecd17dc05bf5918b3293d210538f
Francisco Albarino <francisco@albarino.pt>
2025-07-24 13:44:50 +0100

    13.44_24.07.2025

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,30 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from 'astro-icon'
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  compressHTML: true,
-  site: 'https://cdfutebol.github.io',
-  integrations: [
-    mdx(),
-    icon(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    compress(),
-  ],
-  vite: {
-    css: {
-      preprocessorOptions: {
-        scss: {
-          logger: {
-            warn: () => {},
-          },
+    compressHTML: true,
+    site: "https://dev.albarinolab.pt",
+    integrations: [
+        mdx(),
+        icon(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        compress(),
+    ],
+    vite: {
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    logger: {
+                        warn: () => {},
+                    },
+                },
+            },
         },
-      },
     },
-  },
-})
+});
